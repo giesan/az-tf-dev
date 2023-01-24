de5939d7390d1525a112e16159e20dab94ad006c
Andrej Giesbrecht <andrej.giesbrecht@bechtle.com>
2023-01-24 08:11:51 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# My experiment with Azure Pipelines and Terraform
---
+++ main.tf
@@ -0,0 +1,40 @@
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "andys-examle-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "exampleaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+resource "azurerm_container_registry" "myacr" {
+  name                = "andysacr"
+  sku                 = "Basic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_role_assignment" "enablePulling" {
+  principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.myacr.id
+  skip_service_principal_aad_check = true
+}
